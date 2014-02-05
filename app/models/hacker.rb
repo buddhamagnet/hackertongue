e995ee9997394d1aed63f9391f070f6c38ce17ef
buddhamagnet <buddhamagnet@gmail.com>
2014-02-05 17:30:33 +0000

    remove scope

--- app/models/hacker.rb
+++ app/models/hacker.rb
@@ -1,6 +1,4 @@
 class Hacker < ActiveRecord::Base
-	validates :username, presence: true
-	validates :language, presence: true
-
-	scope :most_popular, -> { group(:language).order('count_id DESC').limit(1).count('id') }
+  validates :username, presence: true
+  validates :language, presence: true
 end
