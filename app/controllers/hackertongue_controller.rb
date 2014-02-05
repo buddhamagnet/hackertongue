099b44ec66fb41a01b73c2f19b22fcb4e7c617fe
buddhamagnet <buddhamagnet@gmail.com>
2014-02-05 17:29:55 +0000

    minor cleanup

--- app/controllers/hackertongue_controller.rb
+++ app/controllers/hackertongue_controller.rb
@@ -1,12 +1,13 @@
 class HackertongueController < ApplicationController
   def index
+    @most_popular = Hacker.most_popular
   end
 
   def create
-  	if hacker = Hacker.where(username: params[:username]).first
-  	  data = "Found hacker in database! Fave language: #{hacker.language}"
-  	else
-  		data = Hublingo.new.lingo(params[:username])
+    if hacker = Hacker.where(username: params[:username]).first
+      data = "Found hacker in database! Fave language: #{hacker.language}"
+    else
+      data = Hublingo.new.lingo(params[:username])
       case data
       when "Sorry that hacker has no repos!"
       when "That hacker doesn't exist baby."
@@ -15,7 +16,7 @@ class HackertongueController < ApplicationController
         Hacker.create(username: params[:username], language: data.to_s)
         data = "Found hacker on Github! Fave language: #{data}"
       end
-  	end
+    end
     redirect_to root_url, notice: data
   end
 
--- app/models/hacker.rb
+++ app/models/hacker.rb
@@ -1,4 +1,6 @@
 class Hacker < ActiveRecord::Base
 	validates :username, presence: true
 	validates :language, presence: true
+
+	scope :most_popular, -> { group(:language).order('count_id DESC').limit(1).count('id') }
 end
