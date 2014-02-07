f31ff8570eea8aa20e3942f921cdfadfca01e38a
buddhamagnet <buddhamagnet@gmail.com>
2014-02-07 03:39:31 -0500

    respond to hublingo exceptions

--- app/controllers/hackertongue_controller.rb
+++ app/controllers/hackertongue_controller.rb
@@ -10,14 +10,16 @@ class HackertongueController < ApplicationController
     if hacker = Hacker.where(username: params[:username]).first
       data = "Found hacker in database! Fave language: #{hacker.language}"
     else
-      data = Hublingo.new.lingo(params[:username])
-      case data
-      when "Sorry that hacker has no repos!"
-      when "That hacker doesn't exist baby."
-      when /Uh oh! Rate limit! Please try again/
-      else
+      begin
+        data = Hublingo.new.lingo(params[:username])
         Hacker.create(username: params[:username], language: data.to_s)
         data = "Found hacker on Github! Fave language: #{data}"
+      rescue HublingoNotFound
+        data = "That hacker doesn't exist baby."
+      rescue HublingoNoPublic
+        data = "Sorry that hacker has no public repos!"
+      rescue HublingoRateLimit => e
+        data = "Uh oh! Rate limit! Please try again at #{e.message}"
       end
     end
     redirect_to root_url, notice: data
