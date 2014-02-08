1d0872ba3b71631910436f673f2b882587927f1e
buddhamagnet <buddhamagnet@gmail.com>
2014-02-08 11:09:05 +0000

    add spec for most popular method which is not working

--- spec/models/hacker_spec.rb
+++ spec/models/hacker_spec.rb
@@ -1,1 +1,19 @@
 require 'spec_helper'
+
+describe Hacker do
+  before :all do
+    Hacker.create!([
+      { username: 'buddhamagnet', language: 'Ruby' },
+      { username: 'buddhamagnet', language: 'Ruby' },
+      { username: 'buddhamagnet', language: 'Ruby' },
+      { username: 'buddhamagnet', language: 'Clojure' },
+      { username: 'buddhamagnet', language: 'Clojure' },
+      { username: 'buddhamagnet', language: 'Go' },
+      { username: 'buddhamagnet', language: 'Perl' }
+    ])
+  end
+
+  it "should return the most popular language" do
+    Hacker.most_popular.should eql("Ruby")
+  end
+end
